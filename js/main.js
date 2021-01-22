090f0187e2828e29c0130a8a73db5b7fac5be95b
nodasgithub <47042341+nodasgithub@users.noreply.github.com>
2021-01-22 18:12:48 -0500

    Chaged the layout of the website

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    // parentElement is .container
+    const mainPage = new MainPage('.container');
+
+    // set all eventlisteners
+    mainPage.setAllEventListeners();
+};
