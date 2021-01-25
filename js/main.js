1b9a17b61cd77a0144f19fce597ca42725321186
nodasgithub <47042341+nodasgithub@users.noreply.github.com>
2021-01-25 02:05:40 -0500

    Added project page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,43 @@ window.onload = function() {
     // set all eventlisteners
     mainPage.setAllEventListeners();
 };
+
+// Made a class MainPage so that it's transferable and maintainable
+class MainPage {
+    constructor(parentElement) {
+        // Set the parent element
+        // In this assignment, it's .container
+        this.parentElement = document.querySelector(parentElement);
+    }
+
+    // This sets all addEventListener
+    setAllEventListeners() {
+        // Set the function for home-block1
+        this.parentElement.querySelector("#home-block1").addEventListener("click", () => this.processHomeBlock1());
+
+        // Set the function for home-block2
+        this.parentElement.querySelector("#home-block2").addEventListener("click", () => this.processHomeBlock2());
+
+        // Set the function for home-block3
+        this.parentElement.querySelector("#home-block3").addEventListener("click", () => this.processHomeBlock3());
+    }
+
+    // This is for home-block1
+    processHomeBlock1() {
+        // Go to the project page
+        window.location.href = "./pages/project.html";
+    }
+
+    // This is for home-block2
+    processHomeBlock2() {
+        // Go to the about me page
+        window.location.href = "./pages/about-me.html";
+    }
+
+    // This is for home-block3
+    // Open the email editor
+    processHomeBlock3() {
+        // Open the mail editor
+        window.location.href = "mailto:shin.my.homepage@gmail.com";
+    }
+}
